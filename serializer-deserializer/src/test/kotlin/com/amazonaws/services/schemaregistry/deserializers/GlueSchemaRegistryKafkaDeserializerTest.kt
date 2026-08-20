2d20acc458319ab213d632da968f978188e0ce16
Jean-Vincent D'Adda <jean-vincent.d-adda@mobsuccess.com>
2026-08-20 12:47:43 +0200

    test(serde): convert the Kafka deserializer and message index finder tests to Kotlin

---
+++ serializer-deserializer/src/test/kotlin/com/amazonaws/services/schemaregistry/deserializers/GlueSchemaRegistryKafkaDeserializerTest.kt
@@ -0,0 +1,194 @@
+/*
+ * Copyright 2020 Amazon.com, Inc. or its affiliates.
+ * Licensed under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.amazonaws.services.schemaregistry.deserializers
+
+import com.amazonaws.services.schemaregistry.common.AWSDeserializerInput
+import com.amazonaws.services.schemaregistry.utils.AWSSchemaRegistryConstants
+import com.amazonaws.services.schemaregistry.utils.nullOf
+import org.junit.jupiter.api.Assertions.assertDoesNotThrow
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.Assertions.assertNull
+import org.junit.jupiter.api.Assertions.assertThrows
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.junit.jupiter.MockitoExtension
+import org.mockito.kotlin.any
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.whenever
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider
+
+/**
+ * Unit tests for testing Kafka specific de-serializer.
+ */
+@ExtendWith(MockitoExtension::class)
+class GlueSchemaRegistryKafkaDeserializerTest {
+    private val configs: MutableMap<String, Any> = HashMap()
+
+    @Mock
+    private lateinit var mockCredProvider: AwsCredentialsProvider
+
+    @BeforeEach
+    fun setup() {
+        configs[AWSSchemaRegistryConstants.AWS_ENDPOINT] = "https://test"
+        configs[AWSSchemaRegistryConstants.AWS_REGION] = "us-west-2"
+        configs[AWSSchemaRegistryConstants.COMPRESSION_TYPE] = "NONE"
+    }
+
+    /**
+     * Test GlueSchemaRegistryKafkaDeserializer instantiation.
+     */
+    @Test
+    fun test_Create() {
+        // Test create with empty constructor
+        val glueSchemaRegistryKafkaDeserializer1 = GlueSchemaRegistryKafkaDeserializer()
+        assertNotNull(glueSchemaRegistryKafkaDeserializer1.credentialProvider)
+
+        // Test create with AWSCredentialsProvider constructor
+        val glueSchemaRegistryKafkaDeserializer2 =
+            GlueSchemaRegistryKafkaDeserializer(this.mockCredProvider, configs)
+        assertNotNull(glueSchemaRegistryKafkaDeserializer2.credentialProvider)
+    }
+
+    @Test
+    fun test_Create_With_Aws_Deserializer() {
+        // Test create with empty constructor
+        val glueSchemaRegistryKafkaDeserializer =
+            GlueSchemaRegistryKafkaDeserializer(this.mockCredProvider, null)
+        glueSchemaRegistryKafkaDeserializer.glueSchemaRegistryDeserializationFacade =
+            GlueSchemaRegistryDeserializationFacade
+                .builder()
+                .credentialProvider(this.mockCredProvider)
+                .configs(configs)
+                .build()
+
+        assertNotNull(glueSchemaRegistryKafkaDeserializer.credentialProvider)
+        assertNotNull(glueSchemaRegistryKafkaDeserializer.glueSchemaRegistryDeserializationFacade)
+    }
+
+    /**
+     * Test GlueSchemaRegistryKafkaDeserializer configure method for empty configuration.
+     */
+    @Test
+    fun test_Configure_Empty_Config() {
+        val glueSchemaRegistryKafkaDeserializer = GlueSchemaRegistryKafkaDeserializer()
+        assertNotNull(glueSchemaRegistryKafkaDeserializer.credentialProvider)
+
+        val configs = HashMap<String, Any>()
+        assertThrows(IllegalArgumentException::class.java) {
+            glueSchemaRegistryKafkaDeserializer.configure(configs, false)
+        }
+    }
+
+    /**
+     * Test GlueSchemaRegistryKafkaDeserializer deserialize method by mocking the dependency.
+     */
+    @Test
+    fun test_Deserialize_Null_Input() {
+        // Mock the dependency
+        val glueSchemaRegistryDeserializationFacade = mock<GlueSchemaRegistryDeserializationFacade>()
+        val glueSchemaRegistryKafkaDeserializer =
+            GlueSchemaRegistryKafkaDeserializer(this.mockCredProvider, null)
+        glueSchemaRegistryKafkaDeserializer.glueSchemaRegistryDeserializationFacade =
+            glueSchemaRegistryDeserializationFacade
+
+        val result = glueSchemaRegistryKafkaDeserializer.deserialize("TestTopic", null)
+        assertNull(result)
+    }
+
+    /**
+     * Test GlueSchemaRegistryKafkaDeserializer deserialize method by mocking the dependency.
+     */
+    @Test
+    fun test_Deserialize() {
+        val expectedObject = Any()
+        // Mock the dependency
+        val glueSchemaRegistryDeserializationFacade = mock<GlueSchemaRegistryDeserializationFacade>()
+        whenever(glueSchemaRegistryDeserializationFacade.deserialize(any<AWSDeserializerInput>()))
+            .thenReturn(expectedObject)
+        val glueSchemaRegistryKafkaDeserializer =
+            GlueSchemaRegistryKafkaDeserializer(this.mockCredProvider, null)
+        glueSchemaRegistryKafkaDeserializer.glueSchemaRegistryDeserializationFacade =
+            glueSchemaRegistryDeserializationFacade
+
+        val deserializedObject =
+            glueSchemaRegistryKafkaDeserializer.deserialize(
+                "TestTopic",
+                byteArrayOf(AWSSchemaRegistryConstants.HEADER_VERSION_BYTE),
+            )
+        assertEquals(expectedObject, deserializedObject)
+    }
+
+    /**
+     * Tests invoking shutdown invokes the internal AWSDeserializer.close method.
+     */
+    @Test
+    fun testClose_callInternalAWSDeserializer_succeeds() {
+        val glueSchemaRegistryDeserializationFacade = mock<GlueSchemaRegistryDeserializationFacade>()
+        val glueSchemaRegistryKafkaDeserializer =
+            GlueSchemaRegistryKafkaDeserializer(this.mockCredProvider, null)
+        glueSchemaRegistryKafkaDeserializer.glueSchemaRegistryDeserializationFacade =
+            glueSchemaRegistryDeserializationFacade
+
+        Mockito.verify(glueSchemaRegistryDeserializationFacade, Mockito.atMost(1)).close()
+    }
+
+    /**
+     * Test GlueSchemaRegistryKafkaDeserializer configure method for null pointer exception by passing null config
+     */
+    @Test
+    fun testConfigure_nullConfig_throwsException() {
+        val glueSchemaRegistryKafkaDeserializer = GlueSchemaRegistryKafkaDeserializer()
+        assertNotNull(glueSchemaRegistryKafkaDeserializer.credentialProvider)
+
+        assertThrows(NullPointerException::class.java) {
+            glueSchemaRegistryKafkaDeserializer.configure(nullOf(), false)
+        }
+    }
+
+    /**
+     * Test GlueSchemaRegistryKafkaDeserializer configure method for positive scenario by passing valid config.
+     */
+    @Test
+    fun testConfigure_validConfig_throwsException() {
+        val glueSchemaRegistryKafkaDeserializer = GlueSchemaRegistryKafkaDeserializer()
+        assertNotNull(glueSchemaRegistryKafkaDeserializer.credentialProvider)
+
+        assertDoesNotThrow { glueSchemaRegistryKafkaDeserializer.configure(configs, false) }
+    }
+
+    /**
+     * Test GlueSchemaRegistryKafkaDeserializer constructor for null pointer exception by passing null config.
+     */
+    @Test
+    fun testConstructor_nullConfig_throwsException() {
+        assertThrows(NullPointerException::class.java) { GlueSchemaRegistryKafkaDeserializer(nullOf()) }
+    }
+
+    /**
+     * Tests invoking close method.
+     */
+    @Test
+    fun testClose_succeeds() {
+        val glueSchemaRegistryKafkaDeserializer = GlueSchemaRegistryKafkaDeserializer()
+        glueSchemaRegistryKafkaDeserializer.configure(configs, false)
+
+        assertDoesNotThrow { glueSchemaRegistryKafkaDeserializer.close() }
+    }
+}
---
+++ serializer-deserializer/src/test/kotlin/com/amazonaws/services/schemaregistry/serializers/protobuf/MessageIndexFinderTest.kt
@@ -0,0 +1,175 @@
+package com.amazonaws.services.schemaregistry.serializers.protobuf
+
+import com.amazonaws.services.schemaregistry.exception.AWSSchemaRegistryException
+import com.amazonaws.services.schemaregistry.tests.protobuf.syntax2.Basic
+import com.amazonaws.services.schemaregistry.tests.protobuf.syntax2.ComplexNestingSyntax2
+import com.amazonaws.services.schemaregistry.tests.protobuf.syntax3.ComplexNestingSyntax3
+import com.amazonaws.services.schemaregistry.utils.nullOf
+import com.google.common.collect.BiMap
+import com.google.protobuf.Descriptors
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+
+class MessageIndexFinderTest {
+    @ParameterizedTest
+    @MethodSource("testBasicProtoFileProvider")
+    fun testGetAll_IdentifiesMessageIndex_ForBasicProtobufSchemas(testCase: ProtobufTestCase) {
+        val schema = testCase.getSchema()
+        val expected = HashMap<String, Int>()
+        val packageName = testCase.getPackage()
+        expected["$packageName.Address"] = 0
+        expected["$packageName.Customer"] = 1
+
+        val actual = getDescriptorNameMap(messageIndexFinder.getAll(schema.file))
+
+        assertEquals(expected, actual, testCase.fileName)
+    }
+
+    @ParameterizedTest
+    @MethodSource("testNestedProtoFileProvider")
+    fun testGetAll_IdentifiesMessageIndex_ForNestedProtobufSchemas(testCase: ProtobufTestCase) {
+        val schema = testCase.getSchema()
+        val expected = HashMap<String, Int>()
+        val packageName = testCase.getPackage()
+        expected["$packageName.A"] = 0
+        expected["$packageName.A.B"] = 1
+        expected["$packageName.A.B.C"] = 2
+        expected["$packageName.A.B.C.J"] = 3
+        expected["$packageName.A.B.C.J.K"] = 4
+        expected["$packageName.A.B.C.X"] = 5
+        expected["$packageName.A.B.C.X.D"] = 6
+        expected["$packageName.A.B.C.X.D.F"] = 7
+        expected["$packageName.A.B.C.X.D.F.M"] = 8
+        expected["$packageName.A.B.C.X.D.G"] = 9
+        expected["$packageName.A.B.C.X.L"] = 10
+        expected["$packageName.A.I"] = 11
+        expected["$packageName.A.X"] = 12
+        expected["$packageName.N"] = 13
+        expected["$packageName.O"] = 14
+        expected["$packageName.O.A"] = 15
+
+        val actual = getDescriptorNameMap(messageIndexFinder.getAll(schema.file))
+
+        assertEquals(expected, actual, testCase.toString())
+    }
+
+    @ParameterizedTest
+    @MethodSource("testDescriptorProvider")
+    fun testGetByDescriptor_IdentifiesMessageIndex_ForGeneratedPOJO(descriptor: Descriptors.Descriptor) {
+        val actual = messageIndexFinder.getByDescriptor(descriptor.file, descriptor)
+
+        // A.B.C.X.D
+        val expected = 6
+
+        assertEquals(expected, actual, descriptor.fullName)
+    }
+
+    @ParameterizedTest
+    @MethodSource("testMissingDescriptorProvider")
+    fun testGetByDescriptor_WhenNonExistentTypePassed_ThrowsSchemaRegistryError(
+        fileDescriptor: Descriptors.FileDescriptor,
+        descriptor: Descriptors.Descriptor,
+    ) {
+        val ex =
+            assertThrows(AWSSchemaRegistryException::class.java) {
+                messageIndexFinder.getByDescriptor(fileDescriptor, descriptor)
+            }
+        val errorMessage = "Provided descriptor is not present in the schema: ${descriptor.fullName}"
+
+        assertEquals(errorMessage, ex.message)
+    }
+
+    @ParameterizedTest
+    @MethodSource("testBasicDescriptorFileProvider")
+    fun testGetByDescriptor_WhenNullsArePassed_ThrowsValidationException(descriptor: Descriptors.Descriptor) {
+        assertThrows(NullPointerException::class.java) {
+            messageIndexFinder.getByDescriptor(descriptor.file, nullOf())
+        }
+
+        assertThrows(NullPointerException::class.java) {
+            messageIndexFinder.getByDescriptor(nullOf(), descriptor)
+        }
+    }
+
+    @ParameterizedTest
+    @MethodSource("testBasicDescriptorFileProvider")
+    fun testGetByIndex_IdentifiesDescriptor_ForGeneratedPOJO(descriptor: Descriptors.Descriptor) {
+        val actual = messageIndexFinder.getByIndex(descriptor.file, 1)
+
+        // Basic.Customer
+        val expected = Basic.Customer.getDescriptor()
+
+        assertEquals(expected, actual, descriptor.fullName)
+    }
+
+    @ParameterizedTest
+    @MethodSource("testBasicDescriptorFileProvider")
+    fun testGetByIndex_WhenNullsArePassed_ThrowsValidationException(descriptor: Descriptors.Descriptor) {
+        assertThrows(NullPointerException::class.java) {
+            messageIndexFinder.getByIndex(descriptor.file, null)
+        }
+
+        assertThrows(NullPointerException::class.java) {
+            messageIndexFinder.getByIndex(nullOf(), 1)
+        }
+    }
+
+    @ParameterizedTest
+    @MethodSource("testMissingDescriptorProvider")
+    fun testGetByIndex_WhenNonExistentIdPassed_ThrowsSchemaRegistryError(
+        fileDescriptor: Descriptors.FileDescriptor,
+        descriptor: Descriptors.Descriptor,
+    ) {
+        val missingIndex = 100
+        val ex =
+            assertThrows(AWSSchemaRegistryException::class.java) {
+                messageIndexFinder.getByIndex(fileDescriptor, missingIndex)
+            }
+        val errorMessage = "No corresponding descriptor found for the index: $missingIndex"
+
+        assertEquals(errorMessage, ex.message)
+    }
+
+    private fun getDescriptorNameMap(descriptorMap: BiMap<Descriptors.Descriptor, Int>): Map<String, Int> = descriptorMap.entries.associate { kv -> kv.key.fullName to kv.value }
+
+    companion object {
+        private val messageIndexFinder = MessageIndexFinder()
+
+        @JvmStatic
+        fun testBasicProtoFileProvider(): List<Arguments> {
+            val testCases = ProtobufTestCaseReader.getTestCasesByNames("Basic.proto")
+            return testCases.map { Arguments.of(it) }
+        }
+
+        @JvmStatic
+        fun testNestedProtoFileProvider(): List<Arguments> {
+            val testCases =
+                ProtobufTestCaseReader.getTestCasesByNames(
+                    "ComplexNestingSyntax3.proto",
+                    "ComplexNestingSyntax2.proto",
+                )
+            return testCases.map { Arguments.of(it) }
+        }
+
+        @JvmStatic
+        fun testBasicDescriptorFileProvider(): List<Arguments> = listOf(Arguments.of(Basic.Customer.getDescriptor()))
+
+        @JvmStatic
+        fun testDescriptorProvider(): List<Arguments> = listOf(
+            Arguments.of(ComplexNestingSyntax3.A.B.C.X.D.getDescriptor()),
+            Arguments.of(ComplexNestingSyntax2.A.B.C.X.D.getDescriptor()),
+        )
+
+        @JvmStatic
+        fun testMissingDescriptorProvider(): List<Arguments> = listOf(
+            Arguments.of(
+                // Search for Basic type Address in ComplexNesting.
+                ComplexNestingSyntax3.getDescriptor().file,
+                Basic.Address.getDescriptor(),
+            ),
+        )
+    }
+}
